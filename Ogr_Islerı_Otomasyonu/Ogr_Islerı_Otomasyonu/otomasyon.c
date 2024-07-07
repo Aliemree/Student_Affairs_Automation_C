0287154e6b574b624eb5a0cda51de1c64eecdae7
Ali Emre <ali.emree.tr@gmail.com>
2024-07-08 01:20:05 +0300

    Your commit message

--- Ogr_Islerı_Otomasyonu/Ogr_Islerı_Otomasyonu/otomasyon.c
+++ Ogr_Islerı_Otomasyonu/Ogr_Islerı_Otomasyonu/otomasyon.c
@@ -2,21 +2,117 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct bolum
+{
+    int BolumID;
+    char bolumAd[30];
+} bolum;
+
 void OgrenciIslemleri()
 {
     // Öğrenci işlemleri burada olacak
 }
+
 void OgretimGorevlisiIslemleri()
 {
     // Öğretim görevlisi işlemleri burada olacak
 }
+
 void DersIslemleri()
 {
     // Ders işlemleri burada olacak
 }
+
+void BolumEkle()
+{
+    system("cls");
+    printf("Bolum Ekleme Islemi..\n");
+    bolum b1;
+    printf("Bolum Adi : ");
+    getchar(); // Önceki girişten kalan newline karakterini tüketir
+    fgets(b1.bolumAd, sizeof(b1.bolumAd), stdin);
+    b1.bolumAd[strcspn(b1.bolumAd, "\n")] = '\0'; // Newline karakterini kaldırır
+
+    FILE* numptr;
+    if (fopen_s(&numptr, "bolumNumaralari.dat", "a+b") != 0) {
+        perror("File opening failed");
+        return;
+    }
+    int numara = 0;
+    while (fread(&numara, sizeof(int), 1, numptr)) {
+        // Dosyadan son numarayı okuyun
+    }
+    numara += 1;
+    fwrite(&numara, sizeof(int), 1, numptr);
+    fclose(numptr);
+    b1.BolumID = numara;
+
+    FILE* ptr;
+    if (fopen_s(&ptr, "bolumler.dat", "a+b") != 0) {
+        perror("File opening failed");
+        return;
+    }
+    fwrite(&b1, sizeof(bolum), 1, ptr);
+    fclose(ptr);
+
+    printf("%d Numarali Bolum Kaydi Tamam...\n", numara);
+}
+
+void BolumListele()
+{
+    system("cls");
+    printf("Bolum Listeleme Islemi..\n");
+    bolum b1;
+
+    FILE* ptr;
+    if (fopen_s(&ptr, "bolumler.dat", "r+b") != 0) {
+        perror("File opening failed");
+        return;
+    }
+
+    printf("%-20s%-30s\n", "BOLUM_ID", "BOLUM_ADI");
+    printf("--------------------------------------------------\n");
+
+    while (fread(&b1, sizeof(bolum), 1, ptr)) {
+        printf("%-20d%-30s\n", b1.BolumID, b1.bolumAd);
+    }
+
+    fclose(ptr);
+}
+
+
+int BolumMenu()
+{
+    int secim;
+    printf("\n\t BOLUM ISLEMLERI...\n\n");
+    printf("\n\t1-Bolum Ekle\n");
+    printf("\n\t2-Bolum Listele\n");
+    printf("\n\t0-PROGRAMI KAPAT \n");
+    printf("\n\t Seciminiz : ");
+    scanf_s("%d", &secim);
+    return secim;
+}
 void BolumIslemleri()
 {
-    // Bölüm işlemleri burada olacak
+    int secim = BolumMenu();
+    while (secim != 0)
+    {
+        switch (secim)
+        {
+        case 1: BolumEkle();
+            break;
+        case 2: BolumListele();
+         
+            break;
+        case 0:
+            break;
+        default: printf("Hatali Seçim Yaptiniz !\n");
+        }
+        secim = BolumMenu();
+        
+    }
+    printf("Bolum Islemlerinden Cikis Yapiliyor !!");
+    return 0;
 }
 void NotIslemleri()
 {
